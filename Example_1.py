05b3c8c6f775e8cfaaf9c1ad7494e363fadb329a
Morgana119 <145613786+Morgana119@users.noreply.github.com>
2024-11-11 07:35:24 -0600

    [FIX] Variable error

--- Example_1.py
+++ Example_1.py
@@ -10,7 +10,7 @@ st.markdown('The data shown below belongs to incident reports in the city of San
 
 mapa = pd.DataFrame()
 mapa ['Date'] = df['Incident Date']
-mapa ['Day'] = df['Incident Day of week']
+mapa ['Day'] = df['Incident Day of Week']
 mapa ['Police District'] = df['Police District']
 mapa ['Date'] = df['Incident Date']
 mapa ['Neighborhood'] = df['Analysis Neighborhood']
