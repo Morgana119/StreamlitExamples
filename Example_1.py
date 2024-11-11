310626abe7d99a72650477a14b3b6fc2b84be8f4
Morgana119 <145613786+Morgana119@users.noreply.github.com>
2024-11-11 07:54:45 -0600

    [ADD] Subset

--- Example_1.py
+++ Example_1.py
@@ -9,16 +9,54 @@ df
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the reat 2018 to 2020.')
 
 mapa = pd.DataFrame()
-mapa ['Date'] = df['Incident Date']
-mapa ['Day'] = df['Incident Day of Week']
-mapa ['Police District'] = df['Police District']
-mapa ['Date'] = df['Incident Date']
-mapa ['Neighborhood'] = df['Analysis Neighborhood']
-mapa ['Incident Category'] = df['Incident Category']
-mapa ['Incident Subcategory'] = df['Incident Subcategory']
-mapa ['Resolution'] = df['Resolution']
-mapa ['lat'] = df['Latitude']
-mapa ['lon'] = df['Longitude']
+mapa['Date'] = df['Incident Date']
+mapa['Day'] = df['Incident Day of Week']
+mapa['Police District'] = df['Police District']
+mapa['Neighborhood'] = df['Analysis Neighborhood']
+mapa['Incident Category'] = df['Incident Category']
+mapa['Incident Subcategory'] = df['Incident Subcategory']
+mapa['Resolution'] = df['Resolution']
+mapa['lat'] = df['Latitude']
+mapa['lon'] = df['Longitude']
 mapa = mapa.dropna()
 
 subset_data2 = mapa
+police_district_input = st.sidebar.multiselect(
+'Police District',
+mapa.groupby('Police District').count().reset_index()['Police District'].tolist())
+if len(police_district_input) > 0:
+    subset_data2 = mapa[mapa['Police District'].isin(police_district_input)]
+
+subset_data1 = subset_data2
+neighborhood_input = st.sidebar.multiselect(
+'Neighborhood',
+subset_data2.groupby('Neighborhood').count().reset_index()['Neighborhood'].tolist())
+if len(neighborhood_input) > 0:
+    subset_data1 = subset_data2[subset_data2['Neighborhood'].isin(neighborhood_input)]
+
+subset_data = subset_data1
+incident_input = st.sidebar.multiselect(
+'Incident Category',
+subset_data1.groupby('Incident Category').count().reset_index()['Incident Category'].tolist())
+if len(incident_input) > 0:
+    subset_data = subset_data1[subset_data1['Incident Category'].isin(incident_input)]
+            
+subset_data    
+    
+st.markdown('It is important to mention that any police district can answer to any incident, the neighborhood in which it happened is not related to the police district.')    
+st.markdown('Crime locations in San Francisco')
+st.map(subset_data)
+st.markdown('Crimes ocurred per day of the week')
+st.bar_chart(subset_data['Day'].value_counts())
+st.markdown('Crimes ocurred per date')
+st.line_chart(subset_data['Date'].value_counts())
+st.markdown('Type of crimes committed')
+st.bar_chart(subset_data['Incident Category'].value_counts())
+
+agree = st.button('Click to see Incident Subcategories')
+if agree:
+    st.markdown('Subtype of crimes committed')
+    st.bar_chart(subset_data['Incident Subcategory'].value_counts())
+
+
+st.markdown('Resolution status')
